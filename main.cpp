8112335257b23f0cb58c459cfb32eb2088638020
carl0545 <carl0545@vandals.uidaho.edu>
2018-04-19 12:19:09 -0700

    created buildKDTree function to transform labeled matrix

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include "mat.h"
 #include <iostream>
 
+void buildKDTree(Matrix*, int, int, int); //Turns the labeled matrix into a KDTree (in a matrix format)
+
 
 using namespace std;
 
@@ -22,6 +24,50 @@ int main(){
   ///////MATRIX INPUT FROM STDIN////////
   label = labeledMatrix.readLabeledRow();
   unlabeledMatrix.read();
+  labeledMatrix.print();
+
+  ////BUILD KDTREE///
+  Matrix kdTree = new Matrix(labeledMatrix);
+  buildKDTree(&kdTree, 1, 0, labeledMatrix.numRows()-1);
+
+  kdTree.print();
+
+
+}
+/*
+* Turns the labeled matrix into a KDTree (in a matrix format)
+* Destroys original matrix
+*/
+void buildKDTree(Matrix *data, int featureNum, int minR, int maxR){
+  if(featureNum >= data->numCols()){
+    cout << "FEATURES ARE OUT" << endl;
+    return;
+  }
+
+  //Sort on specific feature
+  data->sortRowsByCol(featureNum, minR, maxR);
+
+  //Increase feature number
+  featureNum++;
+
+  //Base Cases
+  int range = maxR-minR;
+
+  if(range == 1){
+    return; //do nothing
+  }
+
+  else if(range == 2){
+    return; //do nothing
+  }
+
+  else{
+    buildKDTree(data, featureNum, minR, maxR/2);
+    buildKDTree(data, featureNum, (maxR/2)+1, maxR);
+  }
+
+
+
 
 
 }
